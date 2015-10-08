64118af661284c4865f9e868e9482fe2cb06cb74
wt <39181819@qq.com>
2015-10-08 17:21:07 +0800

    加入产品说明到报表

--- report/bom_structure.py
+++ report/bom_structure.py
@@ -37,6 +37,7 @@ class bom_structure_with_costprice(report_sxw.rml_parse):
             for l in object:
                 res = {}
                 res['pname'] = l.product_id.name
+                res['pdes'] = l.product_id.description
                 res['pcode'] = l.product_id.default_code
                 res['pqty'] = l.product_qty
                 res['uname'] = l.product_uom.name
